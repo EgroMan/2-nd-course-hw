45896dc05cff620eeee3472e1728e9f84784c464
EgroMan <Wahnsinni96@yandex.ru>
2023-06-25 12:53:52 +0300

    Ожидаю коментов)))

--- script.js
+++ script.js
@@ -1,117 +1,96 @@
 // 1
-let min =function(a, b){
-    return (a > b)? a : b;
+const number = [1, 5, 4, 10, 0, 3];
+for (let i = 0; i < number.length; i++) {
+    if (number[i] == 10) break;
+    console.log(number[i]);
 }
-console.log(Math.min(8, 4));
-console.log(Math.min(6, 6));
-
 // 2
+const numbers = [1, 5, 4, 10, 0, 3]
+console.log(numbers.indexOf('4'));
 
-function even_and_odd(n) {
-    if (n % 2 === 0) {
-        console.log('Число четное');
-    } else {
-        console.log('Число нечетное');
-    }
-}
 // 3
-
-function number(num) {
-    return num * num
-}
-
-console.log(number(10))
-
-function inputNumber() {
-    const str = prompt('Введите число')
-    const num = Number(str)
-    if(num >=18 && num <=50){
-        return number(num)
-    }
-    return
-}
-console.log(inputNumber())
+const num = [1, 3, 5, 10, 20]
+num = num.join('');
+console.log(num);
 
 // 4
-
-const year = prompt('Сколько вам лет?');
-if (year < 0) {
-    console.log('Вы ввели не правильное значение');
-} else if  (age <= 12){
-    console.log("Привет друг");
-} else if (age >= 12){
-    console.log("Добро пожаловать");
+let arr = [[1, 1, 1], [1, 1, 1], [1, 1, 1]];
+for (let i = 0; i < 4; i++)
+{
+    arr[i] = [];
+    for (let j = 0; j <= i; j++)
+    {
+        arr[i][j] = j + 1;
+    }
 }
+console.log(arr);
 
 // 5
-
-function entering_numbers() {
-    while (true) {
-        const value = prompt('Ведите число');
-    if (!value) {
-        alert('Одно или оба значения не являются числом');
-        continue
-    }
-    return entering_numbers(value);
-}
-}
-const age = getUserAge();
-if (entering_numbers + NaN) {
-    alert();
-}
+const numOne = [1, 1, 1]
+const newLength = numOne.push(2, 2, 2);
+console.log(newLength);
+console.log(numOne);
 
 // 6
-let cube = (nuber) => {
-    if (isNaN(nuber) === false){
-        let result = nuber**3;
-        return ('${number} в кубе равняеться')
-    } else{
-        return('Переданный параметр не являеться числом')
-    }
-}
-console.log(cube(0));
-console.log(cube(1));
-console.log(cube(2));
-console.log(cube(3));
-console.log(cube(4));
-console.log(cube(5));
-console.log(cube(6));
-console.log(cube(7));
-console.log(cube(8));
-console.log(cube(9));
-console.log(cube(10));
-console.log(cube(wqwqw));
-
+const numTwo = [9, 8, 7, 'a', 6, 5]
+numTwo.sort(function(a,b) {return a - b});
 
 // 7
-const circle1 = {
-    radius: 5,
-    getArea() {
-      return Math.PI * this.radius ** 2;
-    },
-    getPerimeter() {
-      return 2 * Math.PI * this.radius;
-    }
-  };
-  const circle2 = {
-    radius: 10,
-    getArea() {
-      return Math.PI * this.radius ** 2;
-    },
-    getPerimeter() {
-      return 2 * Math.PI * this.radius;
-    }
-  };
 
-//   8
-
-function click(question, answer) {
-    let userAnswer = prompt(question);
-    if (userAnswer === answer) {
-        console.log('угадал');
-    } else {
-        console.log('не угадал')
-    }
+const numFree = [9, 8, 7, 6, 5]
+let text = prompt('Угадай число')
+if (numFree != numFree) {
+    alert('Угадал')
+} else{
+    alert('Не угадал')
 }
-click('Зимой и летом одним цветом', 'Елка')
-click('Приходит зимой борода из ваты', 'дед мороз')
+
+// 8
+const reverse = (str) =>{
+    const iter = (counter, result) =>{
+        if(str.length == result.length){
+            return result;
+        }        
+        return iter(counter - 1, result + str[counter]);
+    }
+    return iter(str.length-1, ''); 
+}
+console.log('Reverse: ' + reverse('abcdef')); 
+
+//9
+const matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+];
+console.log(matrix.join());
+
+// 10
+let summa = [2, 4, 6, 8, 10];
+
+for (let i = 2; i <= 10; i+1) {
+	summa.push(i);
+}
+console.log(summa); 
+
+// 11
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+const squares = nums.map(function (num) {
+  return num * num
+})
+console.log(squares);
+
+// 12
+ getLengthWords(['слово', '', 'слог', 'длинное предложение', 'буква']); // [5, 0, 4, 19, 5]
+const getLengthWords = (arrStr, num) => arrStr.filter(s => s.length === num);
+
+// 13
+function filterPositive(array) {
+    let negativ =[];
+    for(let i = 0; i < array.length; i++){
+        if (array[i] < 0){
+            negativ.push(array[i]);
+        }
+    }
+  }
+  filterPositive([-1, 0, 5, -10, 56]); // => [-1, -10]
+  filterPositive([-25, 25, 0, -1000, -2]); // => [-25, -1000, -2]
