efcff574fea428fcca8b799a1238e072409ddab4
EgroMan <Wahnsinni96@yandex.ru>
2023-06-28 20:33:11 +0300

    Есть ошибки Vs выдает. Нужны пояснения по ним...

--- script.js
+++ script.js
@@ -1,96 +1,79 @@
 // 1
-const number = [1, 5, 4, 10, 0, 3];
-for (let i = 0; i < number.length; i++) {
-    if (number[i] == 10) break;
-    console.log(number[i]);
-}
-// 2
-const numbers = [1, 5, 4, 10, 0, 3]
-console.log(numbers.indexOf('4'));
+let text =('руки базуки')
+console.log(text.toUpperCase());
 
+// 2
+const str = 'Мама мыла раму, мыла акуртно и бережно.'; 
+console.log(str.indexOf('м'),str.indexOf('р'));
 // 3
-let num = [1, 3, 5, 10, 20]
-num = num.join('');
-console.log(num);
+console.log(Math.round(32.58884));
+console.log(Math.floor(32.58884));
+console.log(Math.ceil(32.58884));
+console.log(Math.trunc(32.58884));
 
 // 4
-let arr = [[1, 1, 1], [1, 1, 1], [1, 1, 1]];
-for (let i = 0; i < 4; i++)
-{
-    arr[i] = [];
-    for (let j = 0; j <= i; j++)
-    {
-        arr[i][j] = j + 1;
-    }
-}
-console.log(arr);
-
+// function getMaxOfArray[(52, 53, 49, 77, 21, 32)]; {
+//     return Math.max.apply(null), Math.min.apply(null);
+//     console.log(Math.max(), Math.min());
+// }
 // 5
-const numOne = [1, 1, 1]
-const newLength = numOne.push(2, 2, 2);
-console.log(newLength);
-console.log(numOne);
+function getRandomInt(minValue, maxValue){
+    return Math.random()* maxValue;
+}
+console.log(getRandomInt(1,10))
 
 // 6
-const numTwo = [9, 8, 7, 'a', 6, 5]
-numTwo.sort(function(a,b) {return a - b});
+function randomNumbersArray(length = 12){
+    return Array.fon(Array(length), () => Math.floor(Math.random() *50) +1);
+}
+const randomarr = randomNumbersArray();
 
 // 7
-
-const numFree = [9, 8, 7, 6, 5]
-let text = prompt('Угадай число')
-if (numFree != numFree) {
-    alert('Угадал')
-} else{
-    alert('Не угадал')
+function getRandomItegralNumber (a, b){
+    if(a>b){c=b; b=a; a=c};
+    let arr = [];
+    for(let i=0; i <= (b-a); i++){
+    arr[i]=[Math.random(), i+a];
+    }
+    return arr.sort()[0][1];
 }
 
 // 8
-const reverse = (str) =>{
-    const iter = (counter, result) =>{
-        if(str.length == result.length){
-            return result;
-        }        
-        return iter(counter - 1, result + str[counter]);
-    }
-    return iter(str.length-1, ''); 
-}
-console.log('Reverse: ' + reverse('abcdef')); 
+let currentDate = new Date();
+console.log(currentDate);
 
-//9
-const matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-];
-console.log(matrix.join());
+// 9
+let currentDate2 = new Date();
+currentDate2.setDate(currentDate2.getDate()+ 73);
 
 // 10
-let summa = [2, 4, 6, 8, 10];
-
-for (let i = 2; i <= 10; i+1) {
-	summa.push(i);
-}
-console.log(summa); 
+let myDate = new Date("28 Июня 2023");
+console.log(myDate);
 
 // 11
-const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-const squares = nums.map(function (num) {
-  return num * num
-})
-console.log(squares);
+let arr = ['Яблоко', 'Груша', 'Дыня', 'Виноград', 'Персик', 'Апельсин', 'Мандарин'];
+arr = arr.sort(() => Math.random() - 0.5);
 
-// 12
- getLengthWords(['слово', '', 'слог', 'длинное предложение', 'буква']); // [5, 0, 4, 19, 5]
-const getLengthWords = (arrStr, num) => arrStr.filter(s => s.length === num);
+// let = prompt('Чему равнялся первый элемент массива?');
+// if ('p=0') {
+    
+// } else {
+    
+// }
+// let = prompt('Чему равнялся последний элемент массива?');
+// if ('p=1') {
+    
+// } else {
+    
+// }
 
-// 13
-function filterPositive(array) {
-    let negativ =[];
-    for(let i = 0; i < array.length; i++){
-        if (array[i] < 0){
-            negativ.push(array[i]);
-        }
-    }
-  }
-  filterPositive([-1, 0, 5, -10, 56]); // => [-1, -10]
-  filterPositive([-25, 25, 0, -1000, -2]); // => [-25, -1000, -2]
+alert("интерестные загадки, все засисит от тебя");
+let a = prompt('Чему равнялся первый элемент массива?');
+let b = prompt('Чему равнялся последний элемент массива?');
+if (a==('p=0') || b==('p=1')); {
+    alert('Поздравляю вы дали 2 правильных ответа!!!')
+} else if(a!==('p=0') || b==('p=1'), a==('p=0') || b!==('p=1'),) {
+    alert('Вы были близки к победе!');
+} else if(a!==('p=0') || b!==('p=1'), a!==('p=0') || b!==('p=1'),) {
+    alert('Оба ответка не верны.');
+}
